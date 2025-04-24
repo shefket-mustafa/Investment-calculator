771ea24d6dda4842a417623e32998c6c32bd2d66
shefket-mustafa <miracle_1@abv.bg>
2025-04-24 18:16:32 +0300

    Data is taken from app comp and used

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,6 +1,7 @@
 import './display.css';
 
-export default function Display() {
+export default function Display({ onChange, userInput }) {
+
   return (
       <>
       <div className='display-center'>
@@ -8,22 +9,22 @@ export default function Display() {
 
             <div className='initial-investment'>
                 <p>INITIAL INVESTMENT</p>
-                <input type="number" />
+                <input type="number" required value={userInput.initialInvestment} onChange={(event) => onChange('initialInvestment', event.target.value)}/>
             </div>
 
             <div className='anual-investment'>
                 <p>ANUAL INVESTMENT</p>
-                <input type="number" s/>
+                <input type="number" required value={userInput.annualInvestment} onChange={(event) => onChange('annualInvestment', event.target.value)}/>
             </div>
 
             <div className='expected-return'>
                 <p>EXPECTED RETURN</p>
-                <input type="number" />
+                <input type="number" required value={userInput.expectedReturn} onChange={(event) => onChange('expectedReturn', event.target.value)}/>
             </div>
 
             <div className='DURATION'>
                 <p>DURATION</p>
-                <input type="number" />
+                <input type="number" required value={userInput.duration} onChange={(event) => onChange('duration', event.target.value)}/>
             </div>
         </div>
         </div>
